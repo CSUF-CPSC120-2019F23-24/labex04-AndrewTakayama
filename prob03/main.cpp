b990ee8452d27862c5b635ecda8be8a81cd26e1b
AndrewTakayama <Takayamaa19@avphs.net>
2019-09-14 14:30:35 -0700

    I completed a program which added the predicted 2019 profits to the 2018 profits.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  const long COMPANYX2018 = 2103419277; //Sets up Company X's profit from 2018.
+  const double PERCENTAGE = 0.18;
+  std::cout << "Company X's sales in 2018 was " << COMPANYX2018 << " dollars" << std::endl;
+
+  //Going to calculate Company X's profit in 2019.
+  long nextyearsales = COMPANYX2018 * PERCENTAGE;
+  long predictedsales = nextyearsales + COMPANYX2018;
+  std::cout << "The predicted sales for 2019 are " << predictedsales << " dollars" << std::endl;
+
+  return 0;
+}
